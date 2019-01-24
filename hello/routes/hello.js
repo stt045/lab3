742fd39497e77257fc02dc358c0f9c13d709e5d7
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-01-24 00:40:53 -0800

    finish

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -6,6 +6,9 @@
 exports.view = function(req, res){
   
   var nameToShow = req.params.userName;
+  if(nameToShow == undefined) {
+  	nameToShow = "World";
+  }
   console.log("name is " + nameToShow);
 
   res.render('index', {
--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -17,6 +17,7 @@ function initializePage() {
 	});
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
 
 
@@ -24,22 +25,14 @@ $("a.thumbnail").click(projectClick);
 
 function projectClick(e) {
 	// prevent the page from reloading
-	console.log("Project Clicked");
 	e.preventDefault();
-	// In an event handler, $(this) refers to 
-	// the object that triggered the event
-	$(this).css("background-color", "#7fff00");
 
 	var containingProject = $(this).closest(".project");
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+        $(description).fadeOut();
     }
 }
-
-$("#testjs").click(function(e) {
-	console.log("It worked");
-});
 
