c61258b4d7da4ef38f2b531c317fe7a6fb9245fc
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-01-23 23:10:36 -0800

    Implemented the javascript button correctly

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
@@ -11,8 +12,34 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+
+$("a.thumbnail").click(projectClick);
+
+function projectClick(e) {
+	// prevent the page from reloading
+	console.log("Project Clicked");
+	e.preventDefault();
+	// In an event handler, $(this) refers to 
+	// the object that triggered the event
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
+
+$("#testjs").click(function(e) {
+	console.log("It worked");
+});
+
